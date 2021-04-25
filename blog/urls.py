8743c9f44ec20e4dacc8648201eff29a3d137738
goldenglorys <olusolagloryolamide@gmail.com>
2021-04-25 20:15:40 +0100

    Update blog function added

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView
+from .views import (
+    BlogListView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogUpdateView,
+)
+
 from .models import Post
 
 urlpatterns = [
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/', BlogDetailView.as_view(model=Post), name='post_detail'),
     path('', BlogListView.as_view(), name='home'),
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView 
+from django.views.generic.edit import CreateView, UpdateView 
 from .models import Post
 
 
@@ -18,3 +18,9 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView): 
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
