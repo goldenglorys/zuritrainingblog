09806621c4ce949e2491092c59a45e5bf40b0656
goldenglorys <olusolagloryolamide@gmail.com>
2021-04-25 20:05:20 +0100

    Adding blog posts from the web through the user end

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, BlogCreateView
 from .models import Post
 
 urlpatterns = [
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/', BlogDetailView.as_view(model=Post), name='post_detail'),
     path('', BlogListView.as_view(), name='home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView 
 from .models import Post
 
 
@@ -12,3 +13,8 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post,
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView): 
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
