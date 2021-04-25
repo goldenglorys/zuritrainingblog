c44d767f5bcb7e5c671633acb1cb0b77e8f50b1e
goldenglorys <olusolagloryolamide@gmail.com>
2021-04-25 20:23:57 +0100

    Delete blog post function added

--- blog/urls.py
+++ blog/urls.py
@@ -4,11 +4,13 @@ from .views import (
     BlogDetailView,
     BlogCreateView,
     BlogUpdateView,
+    BlogDeleteView,
 )
 
 from .models import Post
 
 urlpatterns = [
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
     path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/', BlogDetailView.as_view(model=Post), name='post_detail'),
