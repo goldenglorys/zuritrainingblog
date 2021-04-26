532103cc790799c24a8d8634abf481d7c98a2700
goldenglorys <olusolagloryolamide@gmail.com>
2021-04-26 10:14:44 +0100

    Whitenoise for static files configuration

--- config/settings.py
+++ config/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 
-STATIC_ROOT = STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
