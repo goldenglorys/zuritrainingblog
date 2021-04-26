db3f7cb6f2564e3be0ea28eea761783474437f9c
goldenglorys <olusolagloryolamide@gmail.com>
2021-04-26 13:31:51 +0100

    Commit for heroku deployment config

--- config/settings.py
+++ config/settings.py
@@ -84,10 +84,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    "default": env.dj_db_url("DATABASE_URL")
 }
 
 
