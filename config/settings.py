b2e058d280f6db6c7dc855e1a8d1a7949ab236db
goldenglorys <olusolagloryolamide@gmail.com>
2021-04-26 11:58:24 +0100

    reset password

--- config/settings.py
+++ config/settings.py
@@ -136,4 +136,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 
-LOGOUT_REDIRECT_URL = 'home' 
+LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
