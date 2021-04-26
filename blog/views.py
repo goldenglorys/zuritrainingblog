82ab2bfc40d57090c528c2d7ba9a3f8615c13bb4
goldenglorys <olusolagloryolamide@gmail.com>
2021-04-26 12:30:45 +0100

    permossion/authorization for original user to make way around the post and authenticated user to get it

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import (
@@ -8,27 +10,36 @@ from .models import Post
 
 
 # Create your views here.
-class BlogListView(ListView):
+class BlogListView(LoginRequiredMixin ,ListView):
     model = Post
     template_name = 'home.html'
 
 
-class BlogDetailView(DetailView):
+class BlogDetailView(LoginRequiredMixin, DetailView):
     model = Post,
     template_name = 'post_detail.html'
 
-class BlogCreateView(CreateView): 
+class BlogCreateView(LoginRequiredMixin, CreateView): 
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
 
-class BlogUpdateView(UpdateView): 
+class BlogUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView): 
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
 
+    def test_func(self): # new
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class BlogDeleteView(DeleteView): 
+
+class BlogDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView): 
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('home')
+
+    def test_func(self): # new
+        obj = self.get_object()
+        return obj.author == self.request.user
+
